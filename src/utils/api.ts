563cb8a8b4bd23eac62fb5ca8e47f83dc902a9fc
raftouch <rafanelkin@gmail.com>
2025-07-11 17:04:52 +0200

    :bento: update endpoint to test

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,2 +1,2 @@
 export const API_URL =
-  "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/arbresremarquablesparis2011/records";
+  "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/arbresremarquablesparis2011/records?limit=40";
