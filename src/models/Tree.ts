62a6dcde9fc633d5581205567719ccc67b39277a
raftouch <rafanelkin@gmail.com>
2025-07-11 13:22:37 +0200

    :bento: add tree model

---
+++ src/models/Tree.ts
@@ -0,0 +1,16 @@
+export interface Tree {
+  geom_x_y: {
+    lon: number;
+    lat: number;
+  };
+  com_idarbre: number;
+  com_adresse: string;
+  com_arrondissement: string;
+  com_domanialite: string;
+  com_nom_usuel: string;
+  com_nom_latin: string;
+  com_annee_plantation: string;
+  com_resume: string;
+  com_descriptif: string;
+  com_url_photo1: string;
+}
