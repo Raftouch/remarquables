8f2dbc1836c4e8dc607c2741e501e9c77fc58a85
raftouch <rafanelkin@gmail.com>
2025-07-12 16:08:37 +0200

    :construction: retrieve tree id from params

--- src/pages/TreeDetailsPage.tsx
+++ src/pages/TreeDetailsPage.tsx
@@ -1,3 +1,7 @@
+import { useParams } from "react-router-dom";
+
 export default function TreeDetailsPage() {
-  return <div></div>;
+  const { id } = useParams();
+
+  return <div>id : {id}</div>;
 }
