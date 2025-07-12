8c715ac9d15dff93c05e6686e0abf88ad4d6e28b
raftouch <rafanelkin@gmail.com>
2025-07-12 15:49:31 +0200

    :bricks: add not found page

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import "./index.css";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import TreeDetailsPage from "./pages/TreeDetailsPage.tsx";
+import NotFoundPage from "./pages/NotFoundPage.tsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/details/:id", element: <TreeDetailsPage /> },
+  { path: "*", element: <NotFoundPage /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,3 @@
+export default function NotFoundPage() {
+  return <div className="not-found">Page not found 😒</div>;
+}
