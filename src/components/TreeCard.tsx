d0801bcfb7220a08b71beda84cb2ada8a9a8c4d4
raftouch <rafanelkin@gmail.com>
2025-07-12 16:01:07 +0200

    :zap: update tree card with a link to tree details page

--- src/components/TreeCard.tsx
+++ src/components/TreeCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import type { Tree } from "../models/Tree";
 
 interface TreeCardProps {
@@ -7,8 +8,10 @@ interface TreeCardProps {
 export default function TreeCard({ tree }: TreeCardProps) {
   return (
     <li className="tree-unit" key={tree.com_idarbre}>
-      <p>{tree.com_nom_usuel}</p>
-      <img className="tree-img" src={tree.com_url_photo1} />
+      <Link className="tree-unit__link" to={`/details/${tree.com_idarbre}`}>
+        <p>{tree.com_nom_usuel}</p>
+        <img className="tree-unit__img" src={tree.com_url_photo1} />
+      </Link>
     </li>
   );
 }
