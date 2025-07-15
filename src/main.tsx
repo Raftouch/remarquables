cd060b38f0f9cfb4c31441f2ddd790a7e8dd8438
raftouch <rafanelkin@gmail.com>
2025-07-15 19:50:05 +0200

    :bricks: add mappage path to routing & leaflet css import

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import TreeDetailsPage from "./pages/TreeDetailsPage.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
 import { TreesProvider } from "./context/TreesContext.tsx";
+import MapPage from "./pages/MapPage.tsx";
+import "leaflet/dist/leaflet.css";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/details/:id", element: <TreeDetailsPage /> },
   { path: "*", element: <NotFoundPage /> },
+  { path: "/on-map", element: <MapPage /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
