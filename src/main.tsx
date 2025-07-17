fc6b4a2e6d9b1c7b1b7a00bdaf5548497cec2a52
raftouch <rafanelkin@gmail.com>
2025-07-17 15:00:26 +0200

    :zap: update routing

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,14 @@ import NotFoundPage from "./pages/NotFoundPage.tsx";
 import { TreesProvider } from "./context/TreesContext.tsx";
 import MapPage from "./pages/MapPage.tsx";
 import "leaflet/dist/leaflet.css";
+import GroupedTreesPage from "./pages/GroupedTreesPage.tsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/details/:id", element: <TreeDetailsPage /> },
   { path: "*", element: <NotFoundPage /> },
   { path: "/on-map", element: <MapPage /> },
+  { path: "/grouped", element: <GroupedTreesPage /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
---
+++ src/pages/GroupedTreesPage.tsx
@@ -0,0 +1,10 @@
+import GroupedTrees from "../components/GroupedTrees";
+
+export default function GroupedTreesPage() {
+  return (
+    <div>
+      <h1>Grouped Trees</h1>
+      <GroupedTrees />
+    </div>
+  );
+}
