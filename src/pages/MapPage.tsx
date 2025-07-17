4661b4e7d6d386445e2c3c3dad742407f84a865b
raftouch <rafanelkin@gmail.com>
2025-07-17 16:18:49 +0200

    :fire: rm comments

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -24,16 +24,7 @@ export default function MapPage() {
         zoom={13}
         scrollWheelZoom={false}
         style={{ width: "100%", height: "100vh" }}
-        // attributionControl={false}
       >
-        {/* <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; OpenStreetMap contributors"
-        /> */}
-        {/* <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-          attribution="&copy; OpenStreetMap contributors &copy; CARTO"
-        /> */}
         <TileLayer
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           attribution="&copy; OpenStreetMap contributors &copy; CARTO"
