37b5a83b583449876b21ef1fda6464c55fa17347
raftouch <rafanelkin@gmail.com>
2025-07-17 19:36:18 +0200

    :sparkles: add burger menu

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,19 +1,34 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navigation">
       <span>
         <Link to="/">🌳</Link>
       </span>
-      <ul>
+      <ul className={`navigation__nav ${isOpen ? "active" : ""}`}>
         <li>
-          <Link to="/grouped">Vue par nom</Link>
+          <Link to="/grouped" onClick={() => setIsOpen(false)}>
+            Vue par nom
+          </Link>
         </li>
         <li>
-          <Link to="/on-map">Carte</Link>
+          <Link to="/on-map" onClick={() => setIsOpen(false)}>
+            Carte
+          </Link>
         </li>
       </ul>
+
+      <div
+        className={`burger-menu ${isOpen ? "active" : ""}`}
+        onClick={() => setIsOpen(!isOpen)}
+        aria-label="Menu burger"
+      >
+        <span></span>
+      </div>
     </nav>
   );
 }
