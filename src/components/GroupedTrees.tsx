f6fa5f6f6773573484d04352fb7090f3e3f67fa5
raftouch <rafanelkin@gmail.com>
2025-07-17 14:57:06 +0200

    :zap: group trees by name and arrondissement

--- src/components/GroupedTrees.tsx
+++ src/components/GroupedTrees.tsx
@@ -11,28 +11,38 @@ export default function GroupedTrees() {
 
   return (
     <div>
-      {Object.entries(treesGroupedByName).map(([treeName, treesGrouped]) => (
-        <div className="tree-list__grouped" key={treeName}>
-          <h2>{treeName}</h2>
-          <ul className="tree-list">
-            {treesGrouped.map((tree) => (
-              <li className="tree-unit" key={tree.com_idarbre}>
-                <Link
-                  className="tree-unit__link"
-                  to={`/details/${tree.com_idarbre}`}
-                >
-                  <p className="tree-unit__name">{tree.com_arrondissement}</p>
-                  <img
-                    className="tree-unit__img"
-                    src={tree.com_url_photo1}
-                    alt={`Photo de ${tree.com_nom_usuel}`}
-                  />
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      {Object.entries(groupBy(trees, "com_nom_usuel")).map(
+        ([treeName, treesGrouped]) => (
+          <div className="tree-list__grouped" key={treeName}>
+            <h2>{treeName}</h2>
+
+            {Object.entries(groupBy(treesGrouped, "com_arrondissement")).map(
+              ([arrondissement, treesInArr]) => (
+                <div key={arrondissement}>
+                  <h3>Arrondissement : {arrondissement}</h3>
+                  <ul className="tree-list">
+                    {treesInArr.map((tree) => (
+                      <li className="tree-unit" key={tree.com_idarbre}>
+                        <Link
+                          className="tree-unit__link"
+                          to={`/details/${tree.com_idarbre}`}
+                        >
+                          <p className="">{tree.com_nom_latin}</p>
+                          <img
+                            className="tree-unit__img"
+                            src={tree.com_url_photo1}
+                            alt={`Photo de ${tree.com_nom_usuel}`}
+                          />
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )
+            )}
+          </div>
+        )
+      )}
     </div>
   );
 }
