bb72c94042c58b21396c280169b2b3e53af66434
raftouch <rafanelkin@gmail.com>
2025-07-17 16:02:09 +0200

    :boom: display trees on map

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,15 +1,21 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import type { LatLngExpression } from "leaflet";
+import { useContext } from "react";
+import { TreesContext } from "../context/TreesContext";
+import TreeCard from "../components/TreeCard";
 
 export default function MapPage() {
+  const { trees } = useContext(TreesContext);
+
   const position: LatLngExpression = [48.866667, 2.333333];
+
   return (
     <div>
       <MapContainer
         center={position}
         zoom={13}
         scrollWheelZoom={false}
-        style={{ width: "100vw", height: "100vh" }}
+        style={{ width: "100%", height: "100vh" }}
         // attributionControl={false}
       >
         {/* <TileLayer
@@ -24,9 +30,16 @@ export default function MapPage() {
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           attribution="&copy; OpenStreetMap contributors &copy; CARTO"
         />
-        <Marker position={[48.867, 2.333]}>
-          <Popup>Arbre TEST</Popup>
-        </Marker>
+        {trees.map((tree) => (
+          <Marker
+            key={tree.com_idarbre}
+            position={[tree.geom_x_y.lat, tree.geom_x_y.lon]}
+          >
+            <Popup>
+              <TreeCard tree={tree} />
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
