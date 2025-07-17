bcea1c293646d33af544106732ad40c1c1bf66bf
raftouch <rafanelkin@gmail.com>
2025-07-17 16:17:37 +0200

    :lipstick: add custom icon

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,11 +1,19 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import type { LatLngExpression } from "leaflet";
+import L from "leaflet";
 import { useContext } from "react";
 import { TreesContext } from "../context/TreesContext";
 import TreeCard from "../components/TreeCard";
 
 export default function MapPage() {
   const { trees } = useContext(TreesContext);
+
+  const treeIcon = L.divIcon({
+    html: `<div style="font-size: 24px;">🌳</div>`,
+    className: "custom-marker",
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+  });
 
   const position: LatLngExpression = [48.866667, 2.333333];
 
@@ -34,6 +42,7 @@ export default function MapPage() {
           <Marker
             key={tree.com_idarbre}
             position={[tree.geom_x_y.lat, tree.geom_x_y.lon]}
+            icon={treeIcon}
           >
             <Popup>
               <TreeCard tree={tree} />
