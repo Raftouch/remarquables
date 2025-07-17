a18a6261a2515450c3e686cec3da7a5d2dd5a6aa
raftouch <rafanelkin@gmail.com>
2025-07-17 14:34:52 +0200

    :sparkles: apply function to trees to group them by name

--- src/components/GroupedTrees.tsx
+++ src/components/GroupedTrees.tsx
@@ -1,20 +1,38 @@
 import { useContext } from "react";
 import { TreesContext } from "../context/TreesContext";
-import type { Tree } from "../models/Tree";
+import groupBy from "../utils/group";
+import { Link } from "react-router-dom";
 
 export default function GroupedTrees() {
   const { trees } = useContext(TreesContext);
-  const groupedTrees = trees.reduce<Record<string, Tree[]>>((acc, tree) => {
-    const name = tree.com_nom_usuel;
 
-    if (!acc[name]) {
-      acc[name] = [];
-    }
+  const treesGroupedByName = groupBy(trees, "com_nom_usuel");
+  console.log(treesGroupedByName);
 
-    acc[name].push(tree);
-    console.log("acc : ", acc);
-    return acc;
-  }, {});
-
-  return <div></div>;
+  return (
+    <div>
+      {Object.entries(treesGroupedByName).map(([treeName, treesGrouped]) => (
+        <div className="tree-list__grouped" key={treeName}>
+          <h2>{treeName}</h2>
+          <ul className="tree-list">
+            {treesGrouped.map((tree) => (
+              <li className="tree-unit" key={tree.com_idarbre}>
+                <Link
+                  className="tree-unit__link"
+                  to={`/details/${tree.com_idarbre}`}
+                >
+                  <p className="tree-unit__name">{tree.com_arrondissement}</p>
+                  <img
+                    className="tree-unit__img"
+                    src={tree.com_url_photo1}
+                    alt={`Photo de ${tree.com_nom_usuel}`}
+                  />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
