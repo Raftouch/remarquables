d49474fc2ababa14f72bd69cdcee3ee2171f4d96
raftouch <rafanelkin@gmail.com>
2025-07-29 16:56:58 +0200

    :sparkles: add search

--- src/components/TreeList.tsx
+++ src/components/TreeList.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Pagination from "./Pagination";
 import TreeCard from "./TreeCard";
 import { TreesContext } from "../context/TreesContext";
@@ -6,53 +6,84 @@ import { TreesContext } from "../context/TreesContext";
 export default function TreeList() {
   const { trees, loading, error } = useContext(TreesContext);
   const [currentPage, setCurrentPage] = useState(1);
+  const [query, setQuery] = useState("");
 
   const treesPerPage = 20;
 
+  const filteredTrees = trees.filter((tree) =>
+    tree.com_nom_usuel.toLowerCase().includes(query.toLowerCase())
+  );
+
   const startIndex = (currentPage - 1) * treesPerPage;
   const endIndex = currentPage * treesPerPage;
-  const currentTrees = trees.slice(startIndex, endIndex);
+  const currentTrees = filteredTrees.slice(startIndex, endIndex);
 
   const paginate = (pageNr: number) => setCurrentPage(pageNr);
 
   const nextPage = () => setCurrentPage((prev) => prev + 1);
   const prevPage = () => setCurrentPage((prev) => prev - 1);
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [query]);
+
   return (
     <>
+      <input
+        value={query}
+        onChange={handleChange}
+        type="search"
+        placeholder="Rechercher un arbre..."
+        className="search"
+      />
+
       {loading ? (
         <p className="loading">Chargement en cours...</p>
       ) : error ? (
         <p className="error">{error}</p>
       ) : (
         <>
-          <ul className="tree-list">
-            {currentTrees.map((tree) => (
-              <TreeCard tree={tree} />
-            ))}
-          </ul>
-          <div className="pagination__buttons">
-            <button
-              className="btn--prev"
-              onClick={prevPage}
-              disabled={currentPage === 1}
-            >
-              ⏮
-            </button>
-            <Pagination
-              resultsPerPage={treesPerPage}
-              totalResults={trees.length}
-              paginate={paginate}
-              currentPage={currentPage}
-            />
-            <button
-              className="btn--next"
-              onClick={nextPage}
-              disabled={currentPage === Math.ceil(trees.length / treesPerPage)}
-            >
-              ⏭
-            </button>
-          </div>
+          {filteredTrees.length === 0 ? (
+            <p>Aucun arbre trouvé</p>
+          ) : (
+            <>
+              <ul className="tree-list">
+                {currentTrees.map((tree) => (
+                  <TreeCard key={tree.com_idarbre} tree={tree} />
+                ))}
+              </ul>
+              <div className="pagination__buttons">
+                <button
+                  className="btn--prev"
+                  onClick={prevPage}
+                  disabled={currentPage === 1}
+                >
+                  ⏮
+                </button>
+
+                <Pagination
+                  resultsPerPage={treesPerPage}
+                  totalResults={filteredTrees.length}
+                  paginate={paginate}
+                  currentPage={currentPage}
+                />
+                <button
+                  className="btn--next"
+                  onClick={nextPage}
+                  disabled={
+                    currentPage ===
+                    Math.ceil(filteredTrees.length / treesPerPage)
+                  }
+                >
+                  ⏭
+                </button>
+              </div>
+            </>
+          )}
         </>
       )}
     </>
